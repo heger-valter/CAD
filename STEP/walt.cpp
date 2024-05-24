cf409d2e9b612e8d683b9c7b550e38d0cb4627cd
Valter Heger <heger.valter@gmail.com>
2024-05-24 01:55:05 -0400

    adv

---
+++ STEP/walt.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "config.h"
+
+int main(int argc, char **argv) {
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Version " << walt_VERSION_MAJOR << "." << walt_VERSION_MINOR << std::endl;
+	return 0;
+}
